2881ff2392a47f3f4615fa29b0b9576242fc3b82
Anna Kadomtseva <abanyaaleksandrovna@gmail.com>
2020-09-24 16:48:05 +0300

    Создала похожих персонажей

---
+++ js/setup.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var WIZARDS_COUNT = 4;
+var NAMES = ['Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+]
+var SURNAMES = ['да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+]
+var COAT_COLORS = ['rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+]
+var EYES_COLORS = ['black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+]
+
+var setup = document.querySelector('.setup');
+setup.classList.remove('hidden');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var similarListElement = setup.querySelector('.setup-similar-list');
+
+var getRandomItemFromArray = function (array) {
+  var randomElement = array[Math.floor(Math.random() * array.length)];
+  return randomElement;
+};
+
+var generateArray = function (wizardsCount) {
+  var wizardsArray = [];
+  for (var i = 0; i < wizardsCount; i++) {
+    wizardsArray[i] = {
+      name: getRandomItemFromArray(NAMES) + ' ' + getRandomItemFromArray(SURNAMES),
+      coatColor: getRandomItemFromArray(COAT_COLORS),
+      eyesColor: getRandomItemFromArray(EYES_COLORS)
+    };
+  }
+  return wizardsArray;
+};
+
+var makeElement = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  return wizardElement;
+};
+
+var renderWizards = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(makeElement(wizards[i]));
+  }
+  return fragment;
+};
+
+var wizards = generateArray(WIZARDS_COUNT);
+similarListElement.append(renderWizards(wizards));
+
+document.querySelector('.setup-similar').classList.remove('hidden');
